07333b0bf31ac22f89d95b0932b3f7ee603d5f49
Lun <lunwhl@hotmail.com>
2019-01-02 16:17:36 +0800

    fb chat

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -110,7 +110,7 @@
 
 				<div class="fb-customerchat"
 						page_id="2188187484836926"
-						theme_color="#459645"
+						theme_color="#4267b2"
 						logged_in_greeting="Hi! How can we help you?"
 						logged_out_greeting="GoodBye!... Hope to see you soon."
 						minimized="false">

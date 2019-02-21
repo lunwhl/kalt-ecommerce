77f8795581d6a274559585015a72acf69ac7fd81
Lun <lunwhl@hotmail.com>
2019-02-21 11:31:56 +0800

    test1

--- resources/js/pages/Shop.vue
+++ resources/js/pages/Shop.vue
@@ -77,13 +77,7 @@
 	        </div>
 	       </div>
 	     </section>
-         <div class="fb-customerchat"
-            page_id="2188187484836926"
-            theme_color="#4267b2"
-            logged_in_greeting="Hi! How can we help you?"
-            logged_out_greeting="GoodBye!... Hope to see you soon."
-            minimized="false">
-    </div>
+         
          <v-snackbar v-model="snackbar"
         right="right"
         :timeout="timeout"
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -195,6 +195,13 @@
 						</div><!-- Row /- -->
 					</div><!-- Container /- -->
 				</div><!-- Middel Header /- -->	
+				<div class="fb-customerchat"
+            page_id="2188187484836926"
+            theme_color="#4267b2"
+            logged_in_greeting="Hi! How can we help you?"
+            logged_out_greeting="GoodBye!... Hope to see you soon."
+            minimized="false">
+    </div>
 			</div>		
 			
 			

c36209fc0e3ed922adb2fc9d5758d38e5c8ca2aa
Ryan Liew <fatal-fates@hotmail.com>
2019-02-27 22:02:30 +0800

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -163,7 +163,7 @@
 														<li class="dropdown">
 															<a aria-expanded="false" aria-haspopup="true" role="button" class="dropdown-toggle" href="/about">About Us</a>
 														</li>
-														<li><a href="/home/#our-product">Our Products</a></li>
+														<li><a href="/home/#our-product">Latest Products</a></li>
 														<li><a href="/service">Service</a></li>
 														<li><a href="/home/#contact-us">Contact</a></li>
 														<!-- <li><a title="Contact Us" href="contact-us.html">Contact Us</a></li> -->

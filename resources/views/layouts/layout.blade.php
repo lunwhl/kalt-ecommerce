5b5a33214547c0f77060abe3fca9b700343d0b8e
Ryan Liew <fatal-fates@hotmail.com>
2019-03-06 22:21:23 +0800

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -196,11 +196,11 @@
 					</div><!-- Container /- -->
 				</div><!-- Middel Header /- -->	
 				<div class="fb-customerchat"
-		            page_id="2188187484836926"
+		            page_id="1121237594710070"
 		            theme_color="#4267b2"
 		            logged_in_greeting="Hi! How can we help you?"
 		            logged_out_greeting="GoodBye!... Hope to see you soon."
-		            minimized="false">
+		            minimized="true">
 		    	</div>
 			</div>		
 			
